013a8331630235e536ef8283715c6367b8ee40fe
Julexar <viktor.thorin@gmail.com>
2022-09-24 12:54:01 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    token: 'TOKEN HERE',
+    default_prefix: 's!',
+    owners: ['OWNER ID HERE'],
+    presence: {
+        activities: { 
+            name: 'watching over Silex Community',
+            type: 'WATCHING',
+        },
+        status: 'online',
+    },
+};
